9f62cadc343f4c3f20f11d3138a59095a3c9c4a5
Dave Snigier <dave@davesnigier.com>
2024-02-20 12:48:37 -0500

    test removal of yor trace id

--- code/build/s3.tf
+++ code/build/s3.tf
@@ -7,8 +7,6 @@ resource "aws_s3_bucket" "dev_s3" {
 
   tags = {
     Environment          = "Dev"
-    yor_name             = "dev_s3"
-    yor_trace            = "4cb92a50-3c1a-4ba1-aafc-f97ef8bc1edd"
     git_commit           = "a14d80ef64e972e56947320fceb609b5418f91b0"
     git_file             = "code/build/s3.tf"
     git_last_modified_at = "2024-02-20 17:30:52"
